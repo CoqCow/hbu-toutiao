89ffe8f35b110388ce66f33f483884e285006f9c
e <1004337524@qq.com>
2018-07-08 11:53:07 +0800

    ff

--- src/main/java/com/hbu/toutiao/ToutiaoApplication.java
+++ src/main/java/com/hbu/toutiao/ToutiaoApplication.java
@@ -15,6 +15,5 @@ public class ToutiaoApplication {
 	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(ToutiaoApplication.class, args);
-		//new SpringApplicationBuilder(ToutiaoApplication.class).web(true).run(args);
 	}
 }
